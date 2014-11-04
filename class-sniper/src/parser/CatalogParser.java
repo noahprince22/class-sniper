bf699e3d09b531d532b6b3fc6249e2340a20ae59
Michael Goldstein <mjgolds2@illinois.edu>
2014-11-03 23:21:38 -0600

    adding comment

--- class-sniper/src/parser/CatalogParser.java
+++ class-sniper/src/parser/CatalogParser.java
@@ -12,6 +12,12 @@ import shared.Department;
  */
 public abstract class CatalogParser {
 
+	/**
+	 * 
+	 * @param dept the department of the course
+	 * @param number the course number
+	 * @return a Course object with the parsed course data
+	 */
 	abstract Course parseCourse(Department dept, int number);
 	
 }
